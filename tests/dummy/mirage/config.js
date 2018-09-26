776d5c2c4c61f853381d6b883632a96dc6cda53c
Jean-Philippe Roy <jeanphilippe.roy@bell.ca>
2018-09-26 11:24:41 -0400

    Mirage Setup

---
+++ tests/dummy/mirage/config.js
@@ -0,0 +1,116 @@
+export default function() {
+
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+    Note: these only affect routes defined *after* them!
+  */
+
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Shorthand cheatsheet:
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+    http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
+  */
+
+  this.timing = 0;
+
+  this.get('/characters', recordsFor('characters', [
+    filter(['name', 'age', 'evil']),
+    sort(),
+    paginate()
+  ]));
+}
+
+function recordsFor(resourceName, transforms=[]) {
+  return (schema, request) => {
+    let records = schema[resourceName].all();
+
+    transforms.forEach(transform => {
+      records = transform(records, request);
+    });
+
+    return records;
+  };
+}
+
+function filter(properties) {
+  return (records, request) => {
+    let filteredRecords = records;
+
+    properties.forEach(property => {
+      let key = `filter[${property}]`;
+      let value = request.queryParams[key];
+
+      if (value) {
+        let matcher = new RegExp(value, 'gi');
+        filteredRecords = filteredRecords.filter(record => matcher.test(record[property]));
+      }
+    });
+
+    return filteredRecords;
+  };
+}
+
+function sort() {
+  return (records, request) => {
+    let sortedRecords = records;
+
+    const parameter = request.queryParams['sort'];
+
+    if (parameter) {
+      const properties = parameter.trim(',').map(string => ({
+        key: string.replace(/-/, ''),
+        asc: !string.includes('-')
+      }));
+
+      properties.forEach(property => {
+        sortedRecords = sortedRecords.sort(sortObjects(property.key, !property.asc));
+      });
+    }
+
+    return sortedRecords;
+  };
+}
+
+function paginate() {
+  return (records, request) => {
+    let pageSize = request.queryParams['pageSize'];
+    let totalCount = records.models.length;
+    let page = request.queryParams['currentPage'];
+    let start = (page - 1) * pageSize;
+    let end = (page) * pageSize;
+
+    let paginatedRecords = records.slice(start, end);
+
+    paginatedRecords.meta = { totalCount };
+
+    return paginatedRecords;
+  };
+}
+
+function sortObjects(key, reverse) {
+  return function(a, b) {
+    if (typeof a[key] === 'number') {
+      return (a[key] - b[key]);
+    }
+
+    if (a[key] < b[key]) {
+      return reverse ? 1 : -1;
+    }
+
+    if (a[key] > b[key]) {
+      return reverse ? -1 : 1;
+    }
+
+    return 0;
+  };
+}
