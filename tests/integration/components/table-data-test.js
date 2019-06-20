e359234b05c4c2bd335db7bd43dbd19c5311532b
Samuel Beauchemin <samuel.beauchemin@bell.ca>
2019-06-20 11:48:26 -0400

    Add tests (WIP)

--- tests/integration/components/table-data-test.js
+++ tests/integration/components/table-data-test.js
@@ -1,41 +1,125 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module } from 'qunit';
+import { setupRenderingTest, test, skip } from 'ember-qunit';
+import { click, render, pauseTest } from '@ember/test-helpers';
+import { selectChoose, clickTrigger } from 'ember-power-select/test-support/helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('table-data', 'Integration | Component | table-data', {
-  integration: true
-});
+module('Integration | Component | table-data', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
+  test('it renders', async function(assert) {
 
-  this.set('records', []);
+    this.set('records', []);
 
-  this.render(hbs`
-    {{#table-data records=records}}
-      <table>
+    await render(hbs`
+      {{#table-data records=records}}
+        <table>
+          <tr>
+            <td>
+              template block text
+            </td>
+          </tr>
+        </table>
+      {{/table-data}}
+    `);
+
+    assert.equal(this.$('table').length, 1);
+    assert.equal(this.$('table').text().trim(), 'template block text');
+  });
+
+  skip('it throws assertion error without records', async function(assert) {
+    assert.expectAssertion(async () => {
+      await render(hbs`
+        {{#table-data}}
         <tr>
-          <td>
-            template block text
-          </td>
+        <td>
+        template block text
+        </td>
         </tr>
-      </table>
-    {{/table-data}}
-  `);
+        {{/table-data}}`);
+      },
+      /`records`/,
+      'Throws assertion error if records isn\'t passed');
+  });
 
-  assert.equal(this.$('table').length, 1);
-  assert.equal(this.$('table').text().trim(), 'template block text');
-});
+  test('it notifies observers of current page changes', async function(assert) {
+    const TOTAL_COUNT = 40;
 
-test('it throws assertion error without records', function(assert) {
-  assert.expectAssertion(() => {
-    this.render(hbs`
-      {{#table-data}}
-        <tr>
-          <td>
-            template block text
-          </td>
-        </tr>
-      {{/table-data}}`);
-  },
-  /`records`/,
-  'Throws assertion error if records isn\'t passed');
+    let records = [];
+
+    for (let i = 1; i <= TOTAL_COUNT; ++i) {
+      records.push({ id: i });
+    }
+
+    this.set('records', records);
+    this.set('queryObj', {});
+    this.set('totalCount', TOTAL_COUNT);
+    this.set('currentPage', null);
+    this.set('updateCurrentPage', (page) => { this.set('currentPage', page); });
+
+    await render(hbs`
+      {{#table-data
+        records=records
+        queryObj=queryObj
+        totalCount=totalCount
+        notifyCurrentPageObservers=updateCurrentPage
+        as |table-data|
+      }}
+        {{table-data.pagination}}
+        <table>
+          <tr>
+            <td>
+              template block text
+            </td>
+          </tr>
+        </table>
+      {{/table-data}}
+    `);
+
+    await click('.fa.fa-angle-right');
+
+    assert.equal(this.get('currentPage'), 2);
+  });
+
+  test('it notifies observers of page size changes', async function(assert) {
+    this.set('records', []);
+    this.set('queryObj', {});
+    this.set('totalCount', 0);
+    this.set('pageSize', null);
+    this.set('updatePageSize', (pageSize) => { this.set('pageSize', pageSize); });
+
+    await render(hbs`
+      {{#table-data
+        records=records
+        queryObj=queryObj
+        totalCount=totalCount
+        notifyCurrentPageObservers=updateCurrentPage
+        as |table-data|
+      }}
+        {{table-data.pageSize}}
+        <table>
+          <tr>
+            <td>
+              template block text
+            </td>
+          </tr>
+        </table>
+      {{/table-data}}
+    `);
+
+    await pauseTest();
+
+    await clickTrigger();
+    await selectChoose('50');
+
+    assert.equal(this.get('pageSize'), 15);
+  });
+
+  test('it notifies observers of filters changes', function(assert) {
+
+  });
+
+  test('it notifies observers of sorts changes', function(assert) {
+
+  });
 });
