0e908f810931c0b1db64eb45c07cf6ba716a5797
Bobby Mousseau <bobby.mousseau@bell.ca>
2021-02-16 14:56:00 -0500

    bugfix/json-api-simple-filters
    - Fixes code-climate warnings by splitting parse function;

--- addon/utils/query-parsers/json-api.js
+++ addon/utils/query-parsers/json-api.js
@@ -8,32 +8,42 @@ export default EmberObject.extend({
     params['page[number]'] = currentPage;
     params['page[size]'] = pageSize;
 
+    this._parseSorts(params, sorts);
+    this._parseFilters(params, filters, useSimpleFilters);
+
+    return params;
+  },
+  _parseFilters(params, filters, useSimpleFilters) {
+    if (filters.length > 0) {
+      if (useSimpleFilters) {
+        this._parseSimpleFilters(params, filters);
+      } else {
+        this._parseComplexFilters(params, filters);
+      }
+    }
+  },
+  _parseComplexFilters(params, filters){
+    params['filter'] = filters
+      .map(
+        ({ 
+          value, 
+          comparator: { valueForQuery: comparator }, 
+          property: { valueForQuery: key } 
+        }) => `${key} ${comparator} ${value}`)
+      .join(' and ');
+  },
+  _parseSimpleFilters(params, filters){
+    filters.forEach(
+      ({
+        value, 
+        comparator: { valueForQuery: comparator }, 
+        property: { valueForQuery: key } 
+      }) => params[`filter[${key}]`] = `${comparator}:${value}`);
+  },
+  _parseSorts(params, sorts) {
     if (sorts.length > 0) {
       params['sort'] = sorts.map(s => s.direction === 'asc' ? s.key : `-${s.key}`).join(',');
     }
-
-    if (filters.length > 0) {
-      if (useSimpleFilters) {
-        filters.forEach(
-          ({
-            value, 
-            comparator: { valueForQuery: comparator }, 
-            property: { valueForQuery: key } 
-          }) => params[`filter[${key}]`] = `${comparator}:${value}`);
-      } else {
-        params['filter'] = filters
-          .map(
-            ({ 
-              value, 
-              comparator: { valueForQuery: comparator }, 
-              property: { valueForQuery: key } 
-            }) => `${key} ${comparator} ${value}`)
-          .join(' and ');
-      }
-      
-    }
-
-    return params;
   }
 
 });
